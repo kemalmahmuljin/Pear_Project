a452774fab4cb8205d11f8c94afb70415fbc036a
Guillermo Capittini <gcapittini@localhost.localdomain>
2020-03-20 18:09:22 +0100

    pear plot corrected

--- src/plot_result.py
+++ src/plot_result.py
@@ -21,28 +21,28 @@ coords_str = coords_str[coords_str.find('\n') + 1:]
 coords_stream = StringIO(coords_str)
 coords = np.loadtxt(coords_stream)
 
-coeff_file = open("initial_coeff", 'r')
-coeff_str = coeff_file.read().strip()
-coeff_str = coeff_str[coeff_str.find('[') + 1:-2]
+coeff_file = open("final_coeff", 'r')
+coeff_str = coeff_file.read()
+coeff_str = coeff_str[coeff_str.find('\n') + 1:]
 coeff_stream = StringIO(coeff_str)
 coeff = np.loadtxt(coeff_stream)
 
 f_file = open("f_vector", 'r')
-f_str = f_file.read().strip()
-f_str = f_str[f_str.find('[') + 1:-2]
+f_str = f_file.read()
+f_str = f_str[f_str.find('\n') + 1:]
 f_stream = StringIO(f_str)
 f_vector = np.loadtxt(f_stream)
 
-stiff = np.loadtxt("stiff_2", skiprows=1)
+#stiff = np.loadtxt("stiff_2", skiprows=1)
 def plot_coeff(coords, coeff):
     x = coords[:,0]
     y = coords[:,1]
-    triang1 = mtri.Triangulation(x,y)
-    triang2 = mtri.Triangulation(x+0.060,y)
+    triang1 = mtri.Triangulation(x,y, elements.astype(int))
+    triang2 = mtri.Triangulation(x+0.060,y, elements.astype(int))
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1, projection='3d')
     ax.plot_trisurf(triang1, coeff[coeff.shape[0]//2:], cmap='jet')
-    ax.plot_trisurf(triang2, coeff[:coeff.shape[0]//2+1], cmap='jet')
+    ax.plot_trisurf(triang2, coeff[:coeff.shape[0]//2], cmap='jet')
     ax.view_init(elev=90, azim=-90)
     ax.set_xlim(0,0.120)
     ax.set_ylim(0,0.120)
