0159965f5891e829d40dc88c44e2997153d06860
Kemal Mahmuljin <kemal.mahmuljin@student.kuleuven.be>
2020-04-08 21:53:48 +0200

    added correction to stiffness matrix

--- matlab/Generate_stiffnes.m
+++ matlab/Generate_stiffnes.m
@@ -23,38 +23,60 @@ function [matrix1,matrix2]=Generate_stiffnes(elements, coords, S)
             z2 =  coords(n2,2);
             z3 =  coords(n3,2);  
             % Jacobian
-            J = (r2 - r1)*(z3 - z1) - (r3 - r1)*(z2 - z1);
+            J = abs( (r2 - r1)*(z3 - z1) - (r3 - r1)*(z2 - z1) );
             % k
-            k = J*(r1+r2+r3)/6; 
+            k = (r1+r2+r3)/(6*J); 
+            z21 = z2 - z1;
+            z23 = z2 - z3;
+            z31 = z3 - z1;
+            
+            r21 = r2 - r1;
+            r32 = r3 - r2;
+            r31 = r3 - r1;            
+            
             % s11,s12 etc
-            s11u = (S_ur + S_uz) * k ;       
-            s12u = -S_ur * k;        
-            s13u = -S_uz * k;   
-            s22u =  S_ur * k;
-            s33u =  S_uz * k; 
+            s11u = (S_ur*z23^2   + S_uz*r32^2  ) * k;       
+            s12u = (S_ur*z23*z31 - S_uz*r32*r31) * k;        
+            s13u = (S_uz*r32*r21 - S_ur*z23*z21) * k;   
+            s22u = (S_ur*z31^2   + S_uz*r31^2  ) * k;
+            s23u =-(S_ur*z31*z21 + S_uz*r31*r21) * k;
+            s33u = (S_ur*z21^2   + S_uz*r21^2  ) * k; 
 
             matrix1(n1,n1) = matrix1(n1,n1)+s11u;
             matrix1(n1,n2) = matrix1(n1,n2)+s12u;
             matrix1(n1,n3) = matrix1(n1,n3)+s13u;
             matrix1(n2,n1) = matrix1(n2,n1)+s12u;
             matrix1(n2,n2) = matrix1(n2,n2)+s22u;
+            matrix1(n2,n3) = matrix1(n2,n3)+s23u;
             matrix1(n3,n1) = matrix1(n3,n1)+s13u;
+            matrix1(n3,n2) = matrix1(n3,n2)+s23u;
             matrix1(n3,n3) = matrix1(n3,n3)+s33u;
 
             %start for matrix 2
             %s11,s12 etc
-            s11v = (S_vr + S_vz) * k ;       
-            s12v = -S_vr * k;        
-            s13v = -S_vz * k;
-            s22v =  S_vr * k;
-            s33v =  S_vz * k;
+%             s11v = (S_vr + S_vz) * k ;       
+%             s12v = -S_vr * k;        
+%             s13v = -S_vz * k;
+%             s22v =  S_vr * k;
+%             s33v =  S_vz * k;
 
+            s11v = (S_vr*z23^2   + S_vz*r32^2  ) * k;       
+            s12v = (S_vr*z23*z31 - S_vz*r32*r31) * k;        
+            s13v = (S_vz*r32*r21 - S_vr*z23*z21) * k;   
+            s22v = (S_vr*z31^2   + S_vz*r31^2  ) * k;
+            s23v =-(S_vr*z31*z21 + S_vz*r31*r21) * k;
+            s33v = (S_vr*z21^2   + S_vz*r21^2  ) * k;
+            
+            
+            
             matrix2(n1,n1) = matrix2(n1,n1)+s11v;
             matrix2(n1,n2) = matrix2(n1,n2)+s12v;
             matrix2(n1,n3) = matrix2(n1,n3)+s13v;
             matrix2(n2,n1) = matrix2(n2,n1)+s12v;
             matrix2(n2,n2) = matrix2(n2,n2)+s22v;
+            matrix2(n2,n3) = matrix2(n2,n3)+s23v;
             matrix2(n3,n1) = matrix2(n3,n1)+s13v;
+            matrix2(n3,n2) = matrix2(n3,n2)+s23v;
             matrix2(n3,n3) = matrix2(n3,n3)+s33v;
     end
     % matrix_store = matrix1;
--- matlab/test_decay.m
+++ matlab/test_decay.m
@@ -1,24 +1,25 @@
 %% Importing everything
-boundaries = dlmread('output/boundaries', ' ', 1, 0);
+boundaries = dlmread('../output/boundaries', ' ', 1, 0);
 boundaries = boundaries(:, 1:3);
-elements = dlmread('output/elements', ' ', 1, 0);
+elements = dlmread('../output/elements', ' ', 1, 0);
 elements = elements(:, 1:3);
-coords = dlmread('output/coords', ' ', 1, 0);
+coords = dlmread('../output/coords', ' ', 1, 0);
 coords = coords(:, 1:2);
 
-stiffness = dlmread('output/stiff', ' ', 1, 0);
+stiffness = dlmread('../output/stiff', ' ', 1, 0);
 stiffness = stiffness(:, 1:end - 1);
-stiffness_lin = dlmread('output/stiff_lin', ' ', 1, 0);
+stiffness_lin = dlmread('../output/stiff_lin', ' ', 1, 0);
 stiffness_lin = stiffness_lin(:, 1:end - 1);
-fvector = dlmread('output/f_vector', ' ', 1, 0);
+fvector = dlmread('../output/f_vector', ' ', 1, 0);
 fvector = fvector(:, 1:end - 1)';
-fvector_lin = dlmread('output/f_vector_lin', ' ', 1, 0);
+fvector_lin = dlmread('../output/f_vector_lin', ' ', 1, 0);
 fvector_lin = fvector_lin(:, 1:end - 1)';
 
-x0 = dlmread('output/initial_coeff', ' ', 1, 0);
+x0 = dlmread('../output/initial_coeff', ' ', 1, 0);
 x0= x0(:, 1:end - 1)';
 
 %% function decaying with inverse square
+%{
 result_coeff = ones(2*size(coords,1),1)';
 
 for i=1:size(coords,1)
@@ -37,5 +38,18 @@ dlmwrite('output/f_calc_lin',4,'delimiter',' ','precision',12,'-append');
 dlmwrite('output/f_calc_lin',f_calc_lin','delimiter',' ','precision',12,'-append');
 dlmwrite('output/calculated_coeff',4,'delimiter',' ','precision',12,'-append');
 dlmwrite('output/calculated_coeff',result_coeff,'delimiter',' ','precision',12,'-append');
+%}
 
 
+%% function linearly increasing C in r
+%%{
+result_coeff = ones(2*size(coords,1),1)';
+
+for i=1:size(coords,1)
+    r = coords(i,1);
+    result_coeff(i) = 150*r;
+    result_coeff(i + size(coords,1)) = 0.2*r;
+end
+%dlmwrite('../output/calculated_coeff',4,'delimiter',' ','precision',12,'-append');
+dlmwrite('../output/calculated_coeff',result_coeff,'delimiter',' ','precision',12);
+%}
